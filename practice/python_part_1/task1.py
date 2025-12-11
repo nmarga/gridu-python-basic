ea531136bfdd61ba44397a7d2edd490066464607
Nicolae <nmarga@griddynamics.com>
2025-12-11 09:50:49 +0200

    feat(task1): Implemented delete_from_list function

---
+++ practice/python_part_1/task1.py
@@ -0,0 +1,20 @@
+"""
+Write function which deletes defined element from list.
+Restriction: Use .pop method of list to remove item.
+Examples:
+    >>> delete_from_list([1, 2, 3, 4, 3], 3)
+    [1, 2, 4]
+    >>> delete_from_list(['a', 'b', 'c', 'b', 'd'], 'b')
+    ['a', 'c', 'd']
+    >>> delete_from_list([1, 2, 3], 'b')
+    [1, 2, 3]
+    >>> delete_from_list([], 'b')
+    []
+"""
+from typing import List, Any
+
+
+def delete_from_list(list_to_clean: List, item_to_delete: Any) -> List:
+    # Extract all elements from the list that are not equal to item_to_delete
+    # It uses List Comprehensions
+    return [item for item in list_to_clean if item != item_to_delete]
