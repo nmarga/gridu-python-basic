4959ab62ee16caa111efb17305b28b0a19c15b9a
Nicolae <nmarga@griddynamics.com>
2025-12-11 10:01:25 +0200

    feat(task2): Implemented the set_to_dict function

---
+++ practice/python_part_1/task2.py
@@ -0,0 +1,34 @@
+"""
+Write function which updates dictionary with defined values but only if new value more then in dict
+Restriction: do not use .update() method of dictionary
+Examples:
+    >>> set_to_dict({'a': 1, 'b': 2, 'c': 3}, a=0, b=4)  # only b updated because new value for a less then original value
+    {'a': 1, 'b': 4, 'c': 3}
+    >>> set_to_dict({}, a=0)
+    {a: 0}
+    >>> set_to_dict({'a': 5})
+    {'a': 5}
+"""
+from typing import Dict
+
+
+def set_to_dict(dict_to_update: Dict[str, int], **items_to_set) -> Dict:
+
+    # Iterating through each key value in the items_to_set
+    for item in items_to_set:
+
+        # Extracting the values from the items
+        dict_value = dict_to_update.get(item)
+        new_value = items_to_set.get(item)
+
+        # Check if the dict_value actually exist in the dictonary, if not, add it
+        if not dict_value:
+            dict_to_update[item] = new_value
+            # Skip this iteration
+            continue
+
+        # Compare original dictionary value with the new value from the items_to_set parameter
+        if dict_value < new_value:
+            dict_to_update[item] = new_value
+        
+    return dict_to_update
