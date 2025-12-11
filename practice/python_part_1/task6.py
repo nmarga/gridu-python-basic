0185a4a3bd22e5d9c5af2cfe5876494f195f547f
Nicolae <nmarga@griddynamics.com>
2025-12-11 12:46:09 +0200

    feat(task6): Implemented the get_min_max function

---
+++ practice/python_part_1/task6.py
@@ -0,0 +1,34 @@
+"""
+Write function which receives filename and reads file line by line and returns min and mix integer from file.
+Restriction: filename always valid, each line of file contains valid integer value
+Examples:
+    # file contains following lines:
+        10
+        -2
+        0
+        34
+    >>> get_min_max('filename')
+    (-2, 34)
+
+Hint:
+To read file line-by-line you can use this:
+with open(filename) as opened_file:
+    for line in opened_file:
+        ...
+"""
+from typing import Tuple
+
+
+def get_min_max(filename: str) -> Tuple[int, int]:
+
+    # Create the list to store the integers
+    ints = []
+
+    # Read the file in read mode
+    with open(filename, 'r') as opened_file:
+        for line in opened_file:
+            # Append the parsed to integer line
+            ints.append(int(line))
+    
+    # Return the min and max tuple
+    return (min(ints), max(ints))
