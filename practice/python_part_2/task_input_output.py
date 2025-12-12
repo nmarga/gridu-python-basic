a5578de36743c71c55c3db7ad21a4d6f5fcde2bf
Nicolae <nmarga@griddynamics.com>
2025-12-12 10:27:24 +0200

    feat(task_input_output): Implemented the read_number function

---
+++ practice/python_part_2/task_input_output.py
@@ -0,0 +1,56 @@
+"""
+Write function which reads a number from input nth times.
+If an entered value isn't a number, ignore it.
+After all inputs are entered, calculate an average entered number.
+Return string with following format:
+If average exists, return: "Avg: X", where X is avg value which rounded to 2 places after the decimal
+If it doesn't exists, return: "No numbers entered"
+Examples:
+    user enters: 1, 2, hello, 2, world
+    >>> read_numbers(5)
+    Avg: 1.67
+    ------------
+    user enters: hello, world, foo, bar, baz
+    >>> read_numbers(5)
+    No numbers entered
+
+"""
+
+
+def read_numbers(n: int) -> str:
+    """
+    Awaits user input to enter a string of numbers,
+    then computes the average of those numbers
+
+    :param n: Limit of processed numbers
+    :type n: int
+    :return:
+    :rtype: str
+    """
+
+    # Await user input and clean trailing white spaces
+    processed_str = ''.join(input().split())
+
+    # Split the input string into n words/numbers
+    nums_str = processed_str.split(',', n - 1)
+
+    # Store the numbers
+    nums = []
+
+    # Parse each number into a float
+    for num_str in nums_str:
+        try:
+            nums.append(float(num_str))
+        except:
+            pass
+
+    # Get the total count of valid numbers
+    nums_count = len(nums)
+
+    # Compute the result
+    if nums_count == 0:
+        return "No numbers entered"
+    else:
+        nums_avg = sum([num for num in nums]) / nums_count
+
+        return f"Avg: {round(nums_avg, 2)}"
