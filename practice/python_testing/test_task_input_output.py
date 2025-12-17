8d58ebd2683e5a06231b1f2fb5171a72b4b3f9f8
Nicolae <nmarga@griddynamics.com>
2025-12-17 15:26:07 +0200

    feat(tests): Added and fixed tests for read_numbers function

--- practice/python_testing/test_task_input_output.py
+++ practice/python_testing/test_task_input_output.py
@@ -16,24 +16,17 @@ from python_part_2.task_input_output import read_numbers
 
 @patch('builtins.input')
 def test_read_numbers_without_text_input(mock_input):
-    """Test read_numbers() function"""
+    """Test read_numbers function with number input only"""
 
     mock_input.return_value = '1, 2, 3, 4'
     assert read_numbers(3) == 'Avg: 2.0'
 
 @patch('builtins.input')
 def test_read_numbers_with_text_input(mock_input):
-    """Test read_numbers() function"""
+    """Test read_numbers function with text input"""
 
     mock_input.return_value = 'hello, world, foo, bar, baz'
     assert read_numbers(5) == 'No numbers entered'
-
-    mock_input.return_value = '1, 2, 3, 4'
-    assert read_numbers(3) == 'Avg: 2.0'
-
-@patch('builtins.input')
-def test_read_numbers_without_text_input(mock_input):
-    """Test read_numbers() function"""
 
     mock_input.return_value = '1, 2, hello, 2, world'
     assert read_numbers(5) == 'Avg: 1.67'
